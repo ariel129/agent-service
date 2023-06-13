e92f213c334709e99eeb9207f10b94482a8ab5ef
ariel129 <arielespanola12@gmail.com>
2023-06-13 09:13:38 +0800

    test

--- Service/ServiceUpdater.cs
+++ Service/ServiceUpdater.cs
@@ -22,7 +22,7 @@ namespace AgentService.Service
         private static GitHubClient _client;
         static ServiceUpdater()
         {
-            var token = "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"; // use a standard name for the env variable
+            var token = "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"; // use a standard name for the env variable
 
             if (string.IsNullOrWhiteSpace(token))
                 throw new InvalidOperationException("GitHub token not found in environment variables");
